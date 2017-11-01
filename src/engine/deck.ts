a41a3e0d4f2f5d6a1378d512171ac457bfe60562
Andres Villagra <andres_vf@icloud.com>
2017-11-01 15:44:21 -0300

    quick definition of deck.

--- src/engine/deck.ts
+++ src/engine/deck.ts
@@ -5,8 +5,10 @@ export class Deck {
   
   cards: Card[];
 
+  constructor(cards: Card[])
+
   shuffle(): void {
-    rand = RandomService();
+    rand = RandomService(32847923); // make the seed be dependent on the time later. blah.
     var deleteDeck = this.cards.slice(0); //make a copy of the deck
     var newDeck: Card[] = [];
     var n: number = 0;
