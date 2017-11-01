c12494207a294461f8457ae61b73963c572beea6
Andres Villagra <andres_vf@icloud.com>
2017-11-01 15:40:50 -0300

    defined the mana and cost class.

--- src/engine/cost.ts
+++ src/engine/cost.ts
@@ -3,4 +3,19 @@ import { Mana } from './mana';
 export class Cost {
   general: number;
   special: [[Mana, number]];
+
+  constructor(general: number, special:[[Mana, number]])
+
+  constructor(general: number, special:[[number, string]]) {
+    this.general = general;
+    this.special = [];
+    for (var cost of special) {
+      try {
+        var myMana = Mana.match(cost[2]);
+        this.special.append([cost[0]]);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
 }
--- src/engine/mana.ts
+++ src/engine/mana.ts
@@ -1,7 +1,20 @@
 export enum Mana {
   Black,
-  While,
+  White,
   Blue,
   Red,
   Green
+}
+
+namespace Mana {
+  export function match(name: string): Mana {
+    switch(name) {
+      case "black": return Black;
+      case "white": return White;
+      case "blue": return Blue;
+      case "red": return Red;
+      case "green": return Green;
+      default: throw new TypeError("Mana type <"+name+"> not existent.");
+    }
+  }
 }
