223124bd516bc34b6ccf09e42b3fa07e9c6819b3
Andres Villagra <andres_vf@icloud.com>
2017-11-02 13:49:11 -0300

    Changed card to an interface

--- src/engine/card.ts
+++ src/engine/card.ts
@@ -1,11 +1,7 @@
 import { Cost } from './cost';
 
-export class Card {
+export interface Card {
   name: string;
-  cardType: string; // Add types enum later. Or make it a child class?
-  cost: Cost;
   image: string; // Possibly string, or url, might make custom type later.
   description: string;
-  attack: number;
-  defence: number;
 }
