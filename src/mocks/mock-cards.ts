57ce00164bf34a2e2dba99e0de4b36fa30a17b6c
Andres Villagra <andres_vf@icloud.com>
2017-11-02 14:42:10 -0300

    Created a quick mock deck for future testing.

---
+++ src/mocks/mock-cards.ts
@@ -0,0 +1,16 @@
+import { Card } from '../engine/card';
+import { Creature } from '../engine/card-types/creature';
+import { Land } from '../engine/card-types/land';
+import { Mana } from '../engine/mana';
+import { Cost } from '../engine/cost';
+
+export mock_cards: Card[] = [
+  new Land('Island', '', '', Mana.Blue, 1),
+  new Land('Island', '', '', Mana.Blue, 1),
+  new Land('Island', '', '', Mana.Blue, 1),
+  new Land('Island', '', '', Mana.Blue, 1),
+  new Creature('Apprentice Sorcerer', '', '', new Cost(2, [[Mana.Blue, 1]]), 1, 1),
+  new Creature('Apprentice Sorcerer', '', '', new Cost(2, [[Mana.Blue, 1]]), 1, 1),
+  new Creature('Apprentice Sorcerer', '', '', new Cost(2, [[Mana.Blue, 1]]), 1, 1),
+  new Creature('Apprentice Sorcerer', '', '', new Cost(2, [[Mana.Blue, 1]]), 1, 1),
+];
