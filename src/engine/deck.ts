5ab24bddf4885915510ea69b7129221ef9d52c34
Andres Villagra <andres_vf@icloud.com>
2017-11-02 14:18:24 -0300

    Added the draw card method.

--- src/engine/deck.ts
+++ src/engine/deck.ts
@@ -4,20 +4,33 @@ import { Card } from './card';
 export class Deck {
   
   cards: Card[];
+  private rand = RandomService(32847923);
 
-  constructor(cards: Card[])
+  constructor(cards: Card[]) {}
 
-  shuffle(): void {
-    rand = RandomService(32847923); // make the seed be dependent on the time later. blah.
+  public shuffle(): void {
     var deleteDeck = this.cards.slice(0); //make a copy of the deck
     var newDeck: Card[] = [];
     var n: number = 0;
 
-    for (var i=0; i<cards.lenght(); i++) {
-      n = rand.randint(0, deleteDeck.lenght());
+    for (var i=0; i<cards.length(); i++) {
+      n = rand.randint(0, deleteDeck.length());
       newDeck.append(deleteDeck[n]);
       deleteDeck.splice(n, 1);
     }
     this.cards = newDeck;
   }
+
+  public draw(): Card {
+    const index = rand.randint(0, cards.length(););
+    var my_card = this.cards[index];
+    this.remove_card(index);
+    
+    return my_card;
+  }
+  
+  private remove_card(index: number): void {
+    this.cards = this.cards.splice(index, 1);
+  }
+
 }
